fbe5d62358d69c18949e0007ad3cd01560cc3935
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2021-02-16 11:27:29 +0600

    Created register, login, logout end point

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,43 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Register
+Route::get('/register',function(Request $request){
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+        return $user;
+});
+
+
+// Login
+Route::post('/login', function(Request $request){
+    $credentials = $request->only('email', 'password');
+    if(! Auth::attempt($credentials)){
+        throw ValidationException::withMessages([
+            'email' => 'Invalid Credentials',
+        ]);
+    }
+
+    // generate an authenticated session
+    $request->session()->regenerate();
+    return response()->json(null, 201);
+});
+
+// Logout
+Route::post('logout', function(Request $request){
+    //auth('web')->logout();
+    Auth::guard('web')->logout();
+
+    // Invalidated the previous session
+    $request->session()->invalidate();
+    // Now create a new session tokern
+    $request->session()->regenerateToken();
+
+    return response()->json(null,200);
 });
